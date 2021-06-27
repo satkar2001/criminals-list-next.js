82c09479a1f4ca8cfcfea4a7b7d16969da440181
satkar2001 <satkaracharya883@gmail.com>
2021-06-27 21:45:45 +0530

    added about.js content

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,13 @@
 const about = () => {
     return (
         <div>
-            <h2>heih ohlfdo h odjfoao l orerm  eis ipsum hofhleuif ojei </h2>
+            <h3>Crime, the intentional commission of an act usually deemed socially harmful or dangerous and specifically defined, prohibited, and punishable under criminal law.
+
+Most countries have enacted a criminal code in which all of the criminal law can be found, though English law—the source of many other criminal-law systems—remains uncodified. The definitions of particular crimes contained in a code must be interpreted in the light of many principles, some of which may not actually be expressed in the code itself. For example, many legal systems take into account the mental state of the accused person at the time the alleged crime was committed. Most legal systems also classify crimes for the purpose of assigning cases to different types of court. Social changes often result in the adoption of new criminal laws and the obsolescence of older ones.
+
+This article focuses on the definition and classification of crime, how it is measured and detected, the characteristics of offenders, and the various stages of criminal proceedings. The material draws principally from common, or Anglo-American, law, with supplementary treatment of civil-law and other systems, including Islamic, African, and Chinese law. For full treatment of particular legal aspects of crime, see criminal law; civil law; common law; court; police; and procedural law. Particular legal systems are treated in Roman law; Germanic law; Chinese law; Indian law; Sharīʿah (Islamic law); and Soviet law. Aspects related to crime are also addressed in criminal justice; criminology; juvenile justice; parole; prison; and punishment.
+
+ </h3>
         </div>
      );
 }
